d795650848c8a2e7b52cea25965e185686bcede3
Steven Mendoza <smendoza171@gmail.com>
2023-01-11 20:41:10 -0800

    first commit

---
+++ src/App.tsx
@@ -0,0 +1,16 @@
+import { useState } from 'react'
+import './App.css'
+
+function App() {
+
+  return (
+    <div className="App">
+
+      <h1>Vite + React</h1>
+
+
+    </div>
+  )
+}
+
+export default App
