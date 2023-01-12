634740f8a89df5c2f53b459f0d225f4f0f6080a3
Steven Mendoza <smendoza171@gmail.com>
2023-01-11 21:19:09 -0800

    testing react script

---
+++ src/AccessibilityScript .tsx
@@ -0,0 +1,9 @@
+import React, { useEffect } from "react";
+
+
+const AccessibilityScript = () => {
+
+  return null;
+};
+
+export default AccessibilityScript;
--- src/App.tsx
+++ src/App.tsx
@@ -1,16 +1,59 @@
-import { useState } from 'react'
-import './App.css'
+import React, { useEffect } from "react";
+import "./App.css";
+// import AccessibilityScript from './AccessibilityScript '
+
+declare global {
+  interface Window {
+    interdeal: any;
+  }
+}
+
 
 function App() {
+  useEffect(() => {
+
+        window.interdeal = {
+          sitekey: "4b2f3cfa90b07248ac5141e99a47cbd0",
+          Position: "Left",
+          Menulang: "EN",
+          domains: {
+            js: "https://cdn.equalweb.com/",
+            acc: "https://access.equalweb.com/",
+          },
+          btnStyle: {
+            vPosition: ["80%", null],
+            scale: ["0.8", "0.8"],
+            color: {
+              main: "#0a51f2",
+              second: "",
+            },
+            icon: {
+              type: 7,
+              shape: "semicircle",
+              outline: false,
+            },
+          },
+        };
+
+        const script = document.createElement("script");
+        script.src = "https://cdn.equalweb.com/core/4.4.1/accessibility.js";
+        script.defer = true;
+        script.integrity =
+          "sha512-tq2wb4PBHqpUqBTfTG32Sl7oexERId9xGHX2O3yF91IYLII2OwM1gJVBXGbEPaLmfSQrIE+uAOzNOuEUZHHM+g==";
+        script.crossOrigin = "anonymous";
+        script.setAttribute("data-cfasync", "true");
+        document.body.appendChild(script);
+
+  }, []);
 
   return (
     <div className="App">
-
+      {/* <AccessibilityScript/> */}
       <h1>Vite + React</h1>
-
-
     </div>
-  )
+  );
+
+
 }
 
-export default App
+export default App;
